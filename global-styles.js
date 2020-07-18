803e97ffd7155660392f6dbb3ea48310cbdd4246
Jesse Hull <jse.hull@gmail.com>
2020-07-18 17:07:39 -0600

    add font Inter

--- global-styles.js
+++ global-styles.js
@@ -16,6 +16,7 @@ const globalStyles = css`
       ${theme.colors.blueSky} 2%
     );
     color: ${theme.colors.black};
+    font-family: 'Inter', sans-serif;
   }
 
   @font-face {
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,10 @@ const index = () => {
       <Head>
         <title>God in your Garden by Scott Hull</title>
         <link rel='icon' href='/favicon.ico' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <Hero />
       <ImageAndText
